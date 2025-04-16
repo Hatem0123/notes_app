f02e5b8f3f0eb95974d99cb884f3f89421eed980
Hatem0123 <hatemelshazly355@gmail.com>
2025-04-16 17:18:56 +0200

    after adding notes list view

--- lib/views/notes_screen/notes_screen.dart
+++ lib/views/notes_screen/notes_screen.dart
@@ -7,6 +7,7 @@ class Notes_screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add),),
       appBar:  AppBar(title: Text("hello"),actions: [Icon(Icons.search,size: 30,)],elevation: 0.0,),
       body:Expanded(
 
